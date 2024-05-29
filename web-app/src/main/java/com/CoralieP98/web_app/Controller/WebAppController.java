0582253f3115a6a7d42964989290b4a8f3def084
cocomeh <coralie.perez98@orange.fr>
2024-05-29 11:26:38 +0200

    vue nav

--- web-app/src/main/java/com/CoralieP98/web_app/Controller/WebAppController.java
+++ web-app/src/main/java/com/CoralieP98/web_app/Controller/WebAppController.java
@@ -1,4 +1,28 @@
 package com.CoralieP98.web_app.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class WebAppController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        return "redirect:/home";
+    }
+
+    @PostMapping("/signUp")
+    public ModelAndView processRequest(@ModelAttribute("signUpForm") SignUpForm form){
+        userService.registration(form);
+        return new ModelAndView("signIn");
+    }
+
+    @GetMapping("/signUp")
+    public ModelAndView showRegisterForm(){
+        return new ModelAndView("signUp", "signUpForm",new SignUpForm());
+    }
 }
