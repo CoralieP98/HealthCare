1fabf9f83ca290695801938a4044c105bb417e18
cocomeh <coralie.perez98@orange.fr>
2024-06-13 15:29:00 +0200

    crud in progress

--- web-app/src/main/java/com/CoralieP98/web_app/Controller/WebAppController.java
+++ web-app/src/main/java/com/CoralieP98/web_app/Controller/WebAppController.java
@@ -1,5 +1,6 @@
 package com.CoralieP98.web_app.Controller;
 
+import com.CoralieP98.web_app.Model.Patient;
 import com.CoralieP98.web_app.Model.User;
 import com.CoralieP98.web_app.Service.Client.UserFeignClient;
 
@@ -9,12 +10,16 @@ import lombok.RequiredArgsConstructor;
 import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,10 +43,6 @@ public class WebAppController {
         return new ModelAndView("homePage");
     }
 
-    @GetMapping("/patientsPage")
-    public ModelAndView patients(){
-        return new ModelAndView("patients");
-    }
 
     @PostMapping("/signUp")
     public ModelAndView userSignUp(@ModelAttribute("userForm") User userForm){
@@ -60,4 +61,25 @@ public class WebAppController {
         return "redirect:/home";
     }
 
+    @GetMapping("/patientsPage")
+    public ModelAndView patients(){
+        return new ModelAndView("patients");
+    }
+
+    @PostMapping("/createPatient")
+    public String createPatient(Patient patient){
+        userFeignClient.createPatient(patient);
+        return "redirect:/patientsPage";
+    }
+
+    @GetMapping("/patientsList")
+    public String getAllPatient(){
+        userFeignClient.getAllPatients();
+        return "redirect:/patientsPage";
+    }
+
+    @GetMapping("/patients")
+    public ResponseEntity<List<Patient>> getListPatient(){
+        return userFeignClient.getAllPatients();
+    }
 }
--- web-app/src/main/java/com/CoralieP98/web_app/Service/UserServiceImpl.java
+++ web-app/src/main/java/com/CoralieP98/web_app/Service/UserServiceImpl.java
@@ -23,4 +23,5 @@ public class UserServiceImpl implements IUserService{
     public User findByUserName(String email){
         return userFeignClient.getUserByUserName(email).getBody();
     }
+
 }
